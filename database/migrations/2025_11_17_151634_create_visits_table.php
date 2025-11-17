b80eafe2c671f6ab17de8781a3e15b22a9869e91
larissa-felixl <larissa.lima145@aluno.ce.gov.br>
2025-11-17 12:20:01 -0300

    feat(migration): visitas.

---
+++ database/migrations/2025_11_17_151634_create_visits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->time('time');
+            $table->string('address');
+            $table->string('description');
+            //$table->unsignedBigInteger('costumer_id');
+            //$table->foreign('costumer_id')->references('id')->on('costumers')->onDelete('cascade');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visits');
+    }
+};
